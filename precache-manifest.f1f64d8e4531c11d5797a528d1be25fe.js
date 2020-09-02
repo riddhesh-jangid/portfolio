d7d5055baa90f70b65241f3eeb2f7bd38994d01e
snakode <riddhesh710@gmail.com>
2020-09-03 00:02:58 +0530

    Updates

---
+++ precache-manifest.f1f64d8e4531c11d5797a528d1be25fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a011456b432371722e34baa4e09678",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "87a8cb7c0f308aa33e72",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8bc3ce02907415bf663e",
+    "url": "/portfolio/static/css/main.aba68d92.chunk.css"
+  },
+  {
+    "revision": "87a8cb7c0f308aa33e72",
+    "url": "/portfolio/static/js/2.2f0535c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.2f0535c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc3ce02907415bf663e",
+    "url": "/portfolio/static/js/main.c3618287.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
